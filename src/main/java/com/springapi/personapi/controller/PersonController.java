516dd939fa8088c2aeabac2b62d6089bf45f8971
DevSottie <j.vitorsilva.dev@gmail.com>
2021-01-30 13:22:18 -0300

    função delete corrigida

--- src/main/java/com/springapi/personapi/controller/PersonController.java
+++ src/main/java/com/springapi/personapi/controller/PersonController.java
@@ -39,9 +39,9 @@ public class PersonController {
         return personService.findById(id);
     }
 
-    @DeleteMapping("{/id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteById(Long id) throws PersonNotFoundException {
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
         personService.delete(id);
     }
 }
